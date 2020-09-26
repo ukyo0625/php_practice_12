9ae7555ffb2a728c43bd544aca439fc1b32ac51d
kobayashiukyo <u.k.knua0419@gmail.com>
2020-09-26 19:50:32 +0900

    add_totalprice

--- menu.php
+++ menu.php
@@ -28,5 +28,8 @@ class Menu {
   public function getTaxIncludedPrice() {
     return floor($this->price * 1.08);
   }
+  public function getTotalPrice() {
+    return $this->getTaxIncludedPrice() * $this->orderCount;
+  }
 }
 ?>
